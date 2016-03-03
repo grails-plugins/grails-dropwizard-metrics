858767c528080307cc4d50d57ccc80694593da50
Jeff Scott Brown <brownj@ociweb.com>
2016-03-03 13:06:12 -0600

    publish artifact

--- travis-build.sh
+++ travis-build.sh
@@ -7,5 +7,16 @@ filename=$(find build/libs -name "*.jar" | head -1)
 filename=$(basename "$filename")
 
 EXIT_STATUS=0
+echo "Publishing artifact for branch $TRAVIS_BRANCH?"
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+
+  if [[ -n $TRAVIS_TAG ]]; then
+      echo "Publishing artifact"
+      ./gradlew bintrayUpload || EXIT_STATUS=$?
+  else
+      echo "No tag present. Not publishing"
+  fi
+fi
 
 exit $EXIT_STATUS
