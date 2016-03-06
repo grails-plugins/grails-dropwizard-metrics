b5a5e8201af6d20ebd3cbeb61c2fe98f6d2e3750
Jeff Scott Brown <brownj@ociweb.com>
2016-03-06 12:28:35 -0600

    Fill in initial plugin code and docs.

--- src/main/groovy/yammer/metrics/YammerMetricsGrailsPlugin.groovy
+++ src/main/groovy/yammer/metrics/YammerMetricsGrailsPlugin.groovy
@@ -1,7 +1,24 @@
 package yammer.metrics
 
-import grails.plugins.*
+import com.codahale.metrics.Metric
+import com.codahale.metrics.MetricRegistry
+import com.codahale.metrics.MetricSet
+import com.codahale.metrics.Slf4jReporter
+import com.codahale.metrics.health.HealthCheck
+import com.codahale.metrics.health.HealthCheckRegistry
+import com.codahale.metrics.health.jvm.ThreadDeadlockHealthCheck
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet
+import com.codahale.metrics.servlets.HealthCheckServlet
+import com.codahale.metrics.servlets.MetricsServlet
+import grails.plugins.Plugin
+import groovy.util.logging.Slf4j
+import org.springframework.boot.context.embedded.ServletRegistrationBean
 
+import java.util.concurrent.TimeUnit
+
+@Slf4j
 class YammerMetricsGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
@@ -33,4 +50,69 @@ Grails 3 plugin providing convenient access to the Yammer Metrics library.
 
     // Online location of the plugin's browseable source code.
     def scm = [ url: "https://github.com/grails-plugins/grails-yammer-metrics" ]
+
+    Closure doWithSpring() {
+        { ->
+            yammerHealthCheckRegistry(HealthCheckRegistry)
+
+            String healthCheckUri = config.getProperty('grails.yammer.health.uri', String, null)
+            if(healthCheckUri) {
+                yammerHealthCheckServlet(HealthCheckServlet, ref('yammerHealthCheckRegistry'))
+                yammerHealthCheckServletRegistryBean(ServletRegistrationBean, ref('yammerHealthCheckServlet'), healthCheckUri)
+            }
+
+            yammerMetricsRegistry MetricRegistry
+            String metricsUri = config.getProperty('grails.yammer.metrics.uri', String, null)
+            if(metricsUri) {
+                yammerMetricsServlet(MetricsServlet, ref('yammerMetricsRegistry'))
+                yammerMetricsServletRegistryBean(ServletRegistrationBean, ref('yammerMetricsServlet'), metricsUri)
+            }
+
+            yammerGarbageCollectorMetricSet GarbageCollectorMetricSet
+            yammerMemoryUsageGaugeSet MemoryUsageGaugeSet
+            yammerThreadStatesGaugeSet ThreadStatesGaugeSet
+            yammerThreadDeadlockHealthCheck ThreadDeadlockHealthCheck
+        }
+    }
+
+    @Override
+    void doWithApplicationContext() {
+        def metricSetBeans = applicationContext.getBeansOfType(MetricSet)
+        def registry = applicationContext.yammerMetricsRegistry
+        if (metricSetBeans) {
+            for (Map.Entry entry : metricSetBeans) {
+                MetricSet set = entry.value
+                if (!(set instanceof MetricRegistry)) {
+                    String beanName = entry.key
+                    registerMetrics beanName, set, registry
+                }
+            }
+        }
+        def healthCheckBeans = applicationContext.getBeansOfType(HealthCheck)
+        if(healthCheckBeans) {
+            def healthRegistry = applicationContext.yammerHealthCheckRegistry
+            for(Map.Entry entry : healthCheckBeans) {
+                HealthCheck check = entry.value
+                String beanName = entry.key
+                healthRegistry.register beanName, check
+            }
+        }
+        def logReporterFequency = config.getProperty('grails.yammer.metrics.reporterFrequency', Integer, 0)
+        if(logReporterFequency > 0) {
+            Slf4jReporter logbackReporter = Slf4jReporter.forRegistry(registry).outputTo(log)
+                    .convertRatesTo(TimeUnit.SECONDS)
+                    .convertDurationsTo(TimeUnit.MILLISECONDS).build()
+            logbackReporter.start logReporterFequency, TimeUnit.SECONDS
+        }
+    }
+
+    protected void registerMetrics(String beanName, MetricSet metricSet, MetricRegistry registry) {
+        for (Map.Entry<String, Metric> entry : metricSet.metrics.entrySet()) {
+            if (entry.value instanceof MetricSet) {
+                registerMetrics(beanName, entry.value, registry)
+            } else {
+                registry.register "grails.yammer.${beanName}.${entry.key}", entry.value
+            }
+        }
+    }
 }
