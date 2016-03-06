7f5d4568d91e6764a1d41b0f34e4770b6432d371
Jeff Scott Brown <brownj@ociweb.com>
2016-03-06 15:13:50 -0600

    remove question mark

--- travis-build.sh
+++ travis-build.sh
@@ -7,7 +7,7 @@ filename=$(find build/libs -name "*.jar" | head -1)
 filename=$(basename "$filename")
 
 EXIT_STATUS=0
-echo "Publishing artifact for branch $TRAVIS_BRANCH?"
+echo "Publishing artifact for branch $TRAVIS_BRANCH"
 if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
 
