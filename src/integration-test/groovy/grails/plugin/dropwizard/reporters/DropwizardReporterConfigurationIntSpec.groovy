c26ed8fd4bf06d82bb2c8f14682ada6e774b6a32
Jack Frosch <jack.frosch@rgare.com>
2017-10-05 23:13:31 -1000

    New test to verify default Slf4j reporter available (for backward compatibility)

---
+++ src/integration-test/groovy/grails/plugin/dropwizard/reporters/DropwizardReporterConfigurationIntSpec.groovy
@@ -0,0 +1,26 @@
+package grails.plugin.dropwizard.reporters
+
+import grails.core.GrailsApplication
+import grails.test.mixin.integration.Integration
+import spock.lang.Specification
+
+@Integration
+class DropwizardReporterConfigurationIntSpec extends Specification {
+
+    static final String CONSOLE_REPORTER_CONFIG = 'grails.dropwizard.metrics.console-reporter'
+    static final String SLF4_REPORTER_CONFIG = 'grails.dropwizard.metrics.slf4j-reporter'
+
+    GrailsApplication grailsApplication
+
+    void "verify Slf4jReporter available by default"() {
+        expect:
+            !grailsApplication.config[SLF4_REPORTER_CONFIG]
+            grailsApplication.mainContext.containsBean('dropwizardSlf4jReporter')
+    }
+
+    void "verify ConsoleReporter not available by default"() {
+        expect:
+            !grailsApplication.config[CONSOLE_REPORTER_CONFIG]
+            !grailsApplication.mainContext.containsBean('dropwizardConsoleReporter')
+    }
+}
